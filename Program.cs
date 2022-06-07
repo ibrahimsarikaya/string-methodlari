a85e68249af99c15e2ee890a9d5aa54d5820fc16
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-07 14:19:49 +0300

    String Methodları

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace string_methodlari // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
